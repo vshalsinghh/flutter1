45672d1bb85d98572b54956974a1ff6d85ddda3c
xCvishal <vishalsingh2402@gmail.com>
2020-08-15 14:20:11 +0530

    theme provided

--- part1/lib/main.dart
+++ part1/lib/main.dart
@@ -7,12 +7,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcome to Flutter'),
-          ),
-          body: RandomWords()),
+      title: 'My flutter App ',
+      theme: ThemeData(
+        // Add the 3 lines from here...
+        primarySwatch: Colors.white10,
+        accentColor: Colors.red,
+      ),
+      home: RandomWords(),
     );
   }
 }
@@ -24,6 +25,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final TextStyle _biggerFont = TextStyle(fontSize: 18);
 
   _buildSuggestions() {
@@ -37,18 +39,67 @@ class _RandomWordsState extends State<RandomWords> {
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
-          return _buildRow(_suggestions[i]);
+          return _buildRow(_suggestions[index]);
         });
   }
 
+  void _pushSaved() {
+    print('pressed');
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        final tiles = _saved.map((WordPair pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        });
+        final divided = ListTile.divideTiles(
+          context: context,
+          tiles: tiles,
+        ).toList();
+        return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(
+              children: divided,
+            ));
+      },
+    ));
+  }
+
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(pair.asPascalCase, style: _biggerFont),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+      onTap: () => setState(() {
+        alreadySaved ? removeSaved(pair) : _saved.add(pair);
+        print(_saved);
+      }),
     );
+  }
+
+  void removeSaved(pair) {
+    _saved.remove(pair);
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildSuggestions();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.list,
+                ),
+                onPressed: _pushSaved),
+          ],
+        ),
+        body: _buildSuggestions());
   }
 }
